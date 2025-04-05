5e17b07baf99e70da4f7c0d47bab05f9cf2c3f81
Youda008 <youda008@gmail.com>
2025-04-05 19:53:46 +0200

    fixed executable name when creating service

--- deploy-package/CreateService.bat
+++ deploy-package/CreateService.bat
@@ -2,6 +2,6 @@
 set SERVNAME=LhwmLoader
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EventLog\Application\%SERVNAME%" /v "EventMessageFile" /t REG_SZ /d "%~dp0\EventLogMessages.dll"
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EventLog\Application\%SERVNAME%" /v "TypesSupported" /t REG_DWORD /d 7
-sc create "%SERVNAME%" binPath= "%~dp0\%SERVNAME%.exe" start= delayed-auto
+sc create "%SERVNAME%" binPath= "%~dp0\%SERVNAME%Service.exe" start= delayed-auto
 sc start "%SERVNAME%"
 pause
